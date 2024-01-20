aba540cde74919f4db6d8851ee924c32018707c3
paul <paul.lanot@mvi.group>
2024-01-20 16:46:49 +0800

    test

--- script/script.js
+++ script/script.js
@@ -26,3 +26,40 @@ function addItemsToCart() {
 
 }
 
+function checkItemsInCart(value) {
+    console.log(value)
+}
+
+let totalAmount = 0
+
+const firstItem = document.getElementById("firstItem")
+const secondItem = document.getElementById("secondItem")
+const thirdItem = document.getElementById("thirdItem")
+const total = document.getElementById("total")
+
+firstItem.addEventListener("change", () => {
+    firstItem.checked ? totalAmount += parseInt(firstItem.value) : totalAmount -= parseInt(firstItem.value)
+
+    total.innerText = totalAmount
+})
+
+secondItem.addEventListener("change", () => {
+
+    if (secondItem.checked) {
+        totalAmount += parseInt(secondItem.value)
+    }
+    else {
+        totalAmount -= parseInt(secondItem.value)
+    }
+    total.innerText = totalAmount
+})
+
+thirdItem.addEventListener("change", () => {
+    if (thirdItem.checked) {
+        totalAmount += parseInt(thirdItem.value)
+    }
+    else {
+        totalAmount -= parseInt(thirdItem.value)
+    }
+    total.innerText = totalAmount
+})
